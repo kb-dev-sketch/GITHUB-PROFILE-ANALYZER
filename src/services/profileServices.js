f86004b7c11d3a62451b67ea0434836d8ea293d0
kb-dev-sketch <kb61406@gmail.com>
2026-06-01 17:09:04 +0530

    make database

--- src/services/profileServices.js
+++ src/services/profileServices.js
@@ -1,12 +1,1 @@
-const express =require("express");
-const{
-    analyzeProfile,
-    getProfiles,
-    getProfileByUsername,
-} = require('../controllers/profileController');
-
-    const router=express.Router();
-    router.post("/analyze",analyzeProfile);
-    router.get("/",getProfiles);
-    router.get("/:username",getProfileByUsername);
-    module.exports=router;
+ 
