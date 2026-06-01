029c043d82229523f737abab690d296ca2df1331
kb-dev-sketch <kb61406@gmail.com>
2026-06-01 15:39:39 +0530

    handle error

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,19 @@
+function notFoundHandler(req,res,next){
+    const error =new Error(`Route not found - ${req.originalUrl}`);
+    error.statusCode=404;
+    next(error);
+}
+
+function errorHandler(err,req,res,next){
+    const statusCode = err.statusCode || err.response?.statusCode || 500;
+    res.status(statusCode).json({
+        message:err.message || "Internal Server Error",
+        success:false,
+        statusCode
+    });
+}
+
+module.exports={
+    notFoundHandler,
+    errorHandler
+};
