a99c01c7999a8ad3c4bcee47946335d4e79f7ec6
kb-dev-sketch <kb61406@gmail.com>
2026-06-01 15:27:46 +0530

    update &check helath ,connect routes ,middleware

--- src/services/app.js
+++ src/services/app.js
@@ -1,14 +1,33 @@
 require('dotenv').config();
 const express=require('express');
 const cors=require('cors');
-const profileRoutes=require('./routes/profileRoutes');
+const profileRoutes=require('../routes/profileRoutes');
+const { notFoundHandler, errorHandler } = require('./middleware/errorMiddleware');
 
 const app=express();
 app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=>{
-    res.json({message:"Welcome to Github profile Analyzer API"});
+    res.json({message:"Welcome to Github profile Analyzer API",
+        success:true,
+        endpoints:{
+            analyzeProfile:"POST /api/profile/analyze",
+            allProfiles:"GET /api/profile",
+            singleProfile:"GET /api/profile/:username",
+            health:'GET /health'
+        }
+    });
 });
-
+app.get('/health',(req,res)=>{
+    res.json({
+        status:"OK",
+        success:true
+    })
+})
 app.use('/api/profile',profileRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+
 module.exports=app;
